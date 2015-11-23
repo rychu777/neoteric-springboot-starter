9a507d003441d053844e36b778a9aa995170b4d0
dziesio <grzegorz@poznachowski.pl>
2015-11-23 17:09:56 +0100

    Added initial WireMock annotation support

--- springtest/src/main/java/pl/poznachowski/springboot/SpringbootTestApplication.java
+++ springtest/src/main/java/pl/poznachowski/springboot/SpringbootTestApplication.java
@@ -2,10 +2,9 @@ package pl.poznachowski.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.archaius.ArchaiusAutoConfiguration;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication (exclude = {ArchaiusAutoConfiguration.class})
+@SpringBootApplication
 @EnableFeignClients
 public class SpringbootTestApplication {
 
--- springtest/src/test/java/pl/poznachowski/springboot/SampleEndpoint2Test.java
+++ springtest/src/test/java/pl/poznachowski/springboot/SampleEndpoint2Test.java
@@ -2,16 +2,12 @@ package pl.poznachowski.springboot;
 
 import com.neoteric.starter.mongo.test.EmbeddedMongoTest;
 import com.neoteric.starter.test.restassured.ContainerIntegrationTest;
+import com.neoteric.starter.test.wiremock.WireMockTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import pl.poznachowski.springboot.mongo.Person;
 
@@ -19,35 +15,38 @@ import javax.ws.rs.core.Response;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.jayway.restassured.RestAssured.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(value = {SpringbootTestApplication.class, SampleEndpoint2Test.Config.class})
+@SpringApplicationConfiguration(SpringbootTestApplication.class)
 @ContainerIntegrationTest
 @EmbeddedMongoTest(dropCollections = "Person")
-@ActiveProfiles("dev")
+@WireMockTest("testService")
 public class SampleEndpoint2Test {
-
-    @Configuration
-    static class Config {
-
-        // this bean will be injected into the OrderServiceTest class
-        @Bean
-        @Profile("dev")
-        @Primary
-        public TextReturner orderService() {
-            TextReturner orderService = new TextReturner() {
-                @Override
-                public String returnString() {
-                    return "XYZ";
-                }
-            };
-            return orderService;
-        }
-    }
 
     @Autowired
     MongoTemplate mongoTemplate;
+
+    @Test
+    public void testWiremock() throws Exception {
+
+        givenThat(get(urlPathEqualTo("api/v1/test"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "text/plain")
+                        .withBody("Hello world!")));
+
+        do {
+
+        } while (true);
+
+//        when()
+//        .get("api/v1/endpoint")
+//                .then()
+//                .log().all()
+//                .assertThat()
+//                .statusCode(Response.Status.OK.getStatusCode());
+    }
 
     @Test
     public void testName() throws Exception {
